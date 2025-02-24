7ec461ee026e29603b656f2e4a2d94509f6e6e4e
alextekever <alexandre.goncalves@tekever.com>
2025-02-24 12:23:40 +0000

    Changed PrivateRoute

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,9 +1,11 @@
+// src/components/PrivateRoute.tsx
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute: React.FC = () => {
-  const isAuthenticated = localStorage.getItem("isAuthenticated");
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+  const token = localStorage.getItem("access_token");
+  console.log("PrivateRoute token:", token); // Debug log
+  return token ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
